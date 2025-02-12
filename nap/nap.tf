9c7b675944349ba205ee86ecfd497e047f014fac
Akash <60446741+akananth@users.noreply.github.com>
2025-02-12 11:17:52 -0600

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -1,11 +1,26 @@
 resource "helm_release" "nginx-plus-ingress" {
   count = 1
-  name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
-  repository = "https://helm.nginx.com/stable"
-  chart = "nginx-ingress"
-  namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
-  values = [file("./charts/nginx-plus-ingress/values.yaml")]
-  depends_on = [
-    kubernetes_secret.docker-registry
-  ]
+    name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
+    repository = "https://helm.nginx.com/stable"
+    chart = "nginx-ingress"
+    #version = "0.16.2"
+    namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
+    values = [file("./charts/nginx-app-protect/values.yaml")]
+
+    depends_on = [
+      kubernetes_secret.docker-registry
+    ]
+}
+resource "helm_release" "nginx-plus-ingress" {
+  count = 1
+    name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
+    repository = "https://helm.nginx.com/stable"
+    chart = "nginx-ingress"
+    #version = "0.16.2"
+    namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
+    values = [templatefile("./charts/nginx-plus-ingress/values.yaml", { app = "${local.app}"})]
+
+    depends_on = [
+      kubernetes_secret.docker-registry
+    ]
 }
