ff472082b7d722010a7916de9b0a1fa82730f794
Akash <60446741+akananth@users.noreply.github.com>
2025-02-12 09:56:32 -0600

    Update prometheus.tf

--- nap/prometheus.tf
+++ nap/prometheus.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "prometheus" {
-  name = format("%s-pro-%s", lower(local.custom_prefix), lower(local.build_suffix))
+  name = format("%s-pro-%s", local.project_prefix, local.build_suffix)
   repository = "https://prometheus-community.github.io/helm-charts"
   chart = "prometheus"
   namespace = kubernetes_namespace.monitoring.metadata[0].name
