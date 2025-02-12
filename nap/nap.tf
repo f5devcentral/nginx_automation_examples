9175a73e04a964addbeae81ff5c69d795207f8ac
Akash <60446741+akananth@users.noreply.github.com>
2025-02-11 23:28:59 -0600

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -1,13 +1,11 @@
 resource "helm_release" "nginx-plus-ingress" {
-  count      = 1
-  name       = format("%s-nap-%s", lower(local.project_prefix), lower(local.build_suffix))
+  count = 1
+  name = format("%s-nap-%s", lower(local.prefix), lower(local.build_suffix))
   repository = "https://helm.nginx.com/stable"
-  chart      = "nginx-ingress"
-  namespace  = kubernetes_namespace.nginx-ingress.metadata[0].name
-  values     = [file("./charts/nginx-app-protect/values.yaml")]
-
+  chart = "nginx-ingress"
+  namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
+  values = [file("./charts/nginx-plus-ingress/values.yaml")]
   depends_on = [
     kubernetes_secret.docker-registry
   ]
 }
-
