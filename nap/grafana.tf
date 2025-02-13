48e06121e7bdbb0b8daf1f5e38700dc6f0cb2bc5
Akash <60446741+akananth@users.noreply.github.com>
2025-02-13 15:31:05 -0600

    Update grafana.tf

--- nap/grafana.tf
+++ nap/grafana.tf
@@ -3,6 +3,6 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart = "grafana"
   version = "6.50.7"
-  namespace = kubernetes_namespace.monitoring.metadata.name
+  namespace = kubernetes_namespace.monitoring.metadata.name[0]
   values = [file("./charts/grafana/values.yaml")]
 }
