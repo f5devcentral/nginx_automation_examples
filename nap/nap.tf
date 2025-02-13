882252571318686356c9c47da35e0923f9daaf6c
Akash <60446741+akananth@users.noreply.github.com>
2025-02-13 15:01:13 -0600

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -1,5 +1,4 @@
 resource "helm_release" "nginx-plus-ingress" {
-  count = 1
     name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
     repository = "https://helm.nginx.com/stable"
     chart = "nginx-ingress"
