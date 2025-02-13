355cf0791bb49244704d99153bfcd60342e6eee5
Akash <60446741+akananth@users.noreply.github.com>
2025-02-13 15:48:32 -0600

    Update outputs.tf

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -1,17 +1,19 @@
 output "external_name" {
-    value = try(tolist(data.kubernetes_service_v1.nginx-service.status.load_balancer.ingress)[0].hostname, null)
+  value = try(data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname, null)
 }
 
 output "external_port" {
-    value = try(tolist(data.kubernetes_service_v1.nginx-service.spec.port)[0].port, null)
+  value = try(data.kubernetes_service_v1.nginx-service.spec.0.port.0.port, null)
 }
 
 output "origin_source" {
-    value = "nap"
+  value = "nap"
 }
 
 output "nap_deployment_name" {
-    value = try(helm_release.nginx-plus-ingress.name, null)
-    sensitive = true
+  value = helm_release.nginx-plus-ingress.name
+  sensitive = true
 }
+
+
 
