69679afff77f12611b7768f6745f640d61f7a211
Cameron Delano <knowbase@icloud.com>
2025-02-13 18:38:32 -0800

    Storage Class and Prometheus update

--- nap/prometheus.tf
+++ nap/prometheus.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus" {
     name = format("%s-pro-%s", local.project_prefix, local.build_suffix)
     repository = "https://prometheus-community.github.io/helm-charts"
     chart = "prometheus"
-    #version = "15.10.2"
+    #version = "27.3.0"
     namespace = kubernetes_namespace.monitoring.metadata[0].name
     values = [file("./charts/prometheus/values.yaml")]
 }
--- nap/storage.tf
+++ nap/storage.tf
@@ -1,6 +1,9 @@
 resource "kubernetes_storage_class_v1" "aws_csi" {
   metadata {
     name = "ebs-sc"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
   }
   storage_provisioner = "ebs.csi.aws.com"
   reclaim_policy      = "Delete"
