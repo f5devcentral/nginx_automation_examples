18a6e21eb454b9ff81174fd24de13183ff0b1708
Akash <60446741+akananth@users.noreply.github.com>
2025-02-18 10:57:06 -0600

    Update versions.tf

--- nap/versions.tf
+++ nap/versions.tf
@@ -1,27 +1,26 @@
 terraform {
-  required_version = ">= 1.6.0" # Ensures Terraform version is 1.6.0 or later
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.87.0" # Use latest stable AWS provider
+      version = ">= 5.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.35.1" # Use latest stable Kubernetes provider
+      version = ">= 2.23.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.17.0" # Use latest stable Helm provider
+      version = ">= 2.12.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.19.0" # Use latest stable Kubectl provider
+      version = ">= 1.14.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = ">= 3.0.2" # Ensure Docker provider is up-to-date
+      version = ">= 3.0.2"
     }
   }
 }
-
