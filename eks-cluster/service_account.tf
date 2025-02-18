04ee84ad63a44a48c4b431c68289aeb5fe1a887e
Akash <60446741+akananth@users.noreply.github.com>
2025-02-18 13:02:45 -0600

    Update service_account.tf

--- eks-cluster/service_account.tf
+++ eks-cluster/service_account.tf
@@ -1,12 +1,10 @@
-resource "kubectl_manifest" "ebs_csi_sa" {
-  yaml_body = <<YAML
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: ebs-csi-controller-sa
-  namespace: kube-system
-  annotations:
-    eks.amazonaws.com/role-arn: ${aws_iam_role.workernodes.arn}
-YAML
+resource "kubernetes_service_account" "ebs_csi_sa" {
+  metadata {
+    name      = "ebs-csi-controller-sa"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.workernodes.arn
+    }
+  }
 }
 
