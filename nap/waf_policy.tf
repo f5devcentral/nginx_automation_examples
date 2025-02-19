cc279865de3bb8ee8132d15ffa54df95db0827e6
Akash <akashdan90@gmail.com>
2025-02-19 16:01:15 -0600

    waf update

--- nap/waf_policy.tf
+++ nap/waf_policy.tf
@@ -5,7 +5,7 @@ resource "kubernetes_manifest" "waf_policy" {
     apiVersion = "k8s.nginx.org/v1"
     kind       = "Policy"
     metadata = {
-      name = "waf-policy"
+      name      = "waf-policy"
     }
     spec = {
       waf = {
@@ -21,7 +21,16 @@ resource "null_resource" "copy_policy_bundle" {
 
   provisioner "local-exec" {
     command = <<EOT
-      POD_NAME=$(kubectl get pods -n nginx-ingress -l app.kubernetes.io/name=nginx-ingress -o jsonpath='{.items[0].metadata.name}')
+      # Wait for the NGINX pod to be in Running state
+      POD_NAME=""
+      while [ -z "$POD_NAME" ]; do
+        POD_NAME=$(kubectl get pods -n nginx-ingress -l app.kubernetes.io/name=nginx-ingress -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
+        sleep 2
+      done
+      
+      kubectl wait --for=condition=Ready pod/$POD_NAME -n nginx-ingress --timeout=60s
+
+      # Copy the compiled WAF policy to the NGINX pod
       kubectl cp ${path.module}/compiled_policy.tgz nginx-ingress/$POD_NAME:/etc/app_protect/bundles/compiled_policy.tgz
     EOT
   }
