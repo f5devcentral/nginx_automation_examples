adea1c865e2476c905f2c03bd5296db73f5da780
Akash <60446741+akananth@users.noreply.github.com>
2025-02-19 09:41:08 -0600

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx-plus-ingress" {
   name       = format("%s-nap-%s", local.project_prefix, local.build_suffix)
   repository = "https://helm.nginx.com/stable"
   chart      = "nginx-ingress"
-  version = "1.4.2" # Uncomment and specify a version for stability
+  version = "2.0.1" # Uncomment and specify a version for stability
   namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
   values     = [file("./charts/nginx-app-protect/values.yaml")]
   depends_on = [
