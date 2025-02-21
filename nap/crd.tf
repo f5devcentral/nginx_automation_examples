64aa459afc288c397c37b399a557dd5d5971ec10
Akash <akashdan90@gmail.com>
2025-02-21 17:11:41 -0600

    crd

--- nap/crd.tf
+++ nap/crd.tf
@@ -1,340 +1,37 @@
-resource "kubectl_manifest" "dnsendpoints_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: dnsendpoints.externaldns.nginx.org
-spec:
-  group: externaldns.nginx.org
-  names:
-    kind: DNSEndpoint
-    listKind: DNSEndpointList
-    plural: dnsendpoints
-    singular: dnsendpoint
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    schema:
-      openAPIV3Schema:
-        description: DNSEndpoint is the CRD wrapper for Endpoint
-        properties:
-          apiVersion:
-            description: API version of the object
-            type: string
-          kind:
-            description: Kind of the object
-            type: string
-          metadata:
-            type: object
-          spec:
-            description: DNSEndpointSpec holds information about endpoints.
-            properties:
-              endpoints:
-                type: array
-                items:
-                  type: object
-                  properties:
-                    dnsName:
-                      type: string
-                    labels:
-                      type: object
-                      additionalProperties:
-                        type: string
-                    providerSpecific:
-                      type: array
-                      items:
-                        type: object
-                        properties:
-                          name:
-                            type: string
-                          value:
-                            type: string
-                    recordTTL:
-                      type: integer
-                      format: int64
-                    recordType:
-                      type: string
-                    targets:
-                      type: array
-                      items:
-                        type: string
-            type: object
-          status:
-            properties:
-              observedGeneration:
-                type: integer
-                format: int64
-            type: object
-        type: object
-    subresources:
-      status: {}
-YAML
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3.4.0"
+    }
+  }
 }
 
-resource "kubectl_manifest" "globalconfigurations_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: globalconfigurations.k8s.nginx.org
-spec:
-  group: k8s.nginx.org
-  names:
-    kind: GlobalConfiguration
-    listKind: GlobalConfigurationList
-    plural: globalconfigurations
-    singular: globalconfiguration
-    shortNames: ["gc"]
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    schema:
-      openAPIV3Schema:
-        description: GlobalConfiguration defines the GlobalConfiguration resource.
-        properties:
-          spec:
-            properties:
-              listeners:
-                type: array
-                items:
-                  type: object
-                  properties:
-                    ipv4:
-                      type: string
-                    ipv6:
-                      type: string
-                    name:
-                      type: string
-                    port:
-                      type: integer
-                    protocol:
-                      type: string
-                    ssl:
-                      type: boolean
-            type: object
-        type: object
-YAML
+provider "kubectl" {
+  # Configure your Kubernetes cluster access
+  # Example configurations:
+  # config_path = "~/.kube/config"
+  # host                   = module.eks.cluster_endpoint
+  # cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  # token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-resource "kubectl_manifest" "policies_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: policies.k8s.nginx.org
-spec:
-  group: k8s.nginx.org
-  names:
-    kind: Policy
-    listKind: PolicyList
-    plural: policies
-    singular: policy
-    shortNames: ["pol"]
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    additionalPrinterColumns:
-    - jsonPath: .status.state
-      name: State
-      type: string
-    - jsonPath: .metadata.creationTimestamp
-      name: Age
-      type: date
-    schema:
-      openAPIV3Schema:
-        properties:
-          spec:
-            properties:
-              accessControl:
-                properties:
-                  allow:
-                    type: array
-                    items:
-                      type: string
-                  deny:
-                    type: array
-                    items:
-                      type: string
-                type: object
-              waf:
-                properties:
-                  apPolicy:
-                    type: string
-                  enable:
-                    type: boolean
-                type: object
-            type: object
-          status:
-            properties:
-              state:
-                type: string
-            type: object
-        type: object
-    subresources:
-      status: {}
-YAML
+data "http" "nginx_crds" {
+  url = "https://raw.githubusercontent.com/nginx/kubernetes-ingress/v4.0.1/deploy/crds.yaml"
 }
 
-resource "kubectl_manifest" "transportservers_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: transportservers.k8s.nginx.org
-spec:
-  group: k8s.nginx.org
-  names:
-    kind: TransportServer
-    listKind: TransportServerList
-    plural: transportservers
-    shortNames: ["ts"]
-    singular: transportserver
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    additionalPrinterColumns:
-    - jsonPath: .status.state
-      name: State
-      type: string
-    - jsonPath: .status.reason
-      name: Reason
-      type: string
-    - jsonPath: .metadata.creationTimestamp
-      name: Age
-      type: date
-    schema:
-      openAPIV3Schema:
-        properties:
-          spec:
-            properties:
-              host:
-                type: string
-              upstreams:
-                type: array
-                items:
-                  type: object
-                  properties:
-                    name:
-                      type: string
-                    service:
-                      type: string
-            type: object
-          status:
-            properties:
-              state:
-                type: string
-            type: object
-        type: object
-    subresources:
-      status: {}
-YAML
-}
+resource "kubectl_manifest" "nginx_crds" {
+  for_each   = { for idx, doc in split("---", data.http.nginx_crds.response_body) : idx => doc if doc != "" }
+  yaml_body  = each.value
+  apply_only = true
 
-resource "kubectl_manifest" "virtualserverroute_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: virtualserverroutes.k8s.nginx.org
-spec:
-  group: k8s.nginx.org
-  names:
-    kind: VirtualServerRoute
-    listKind: VirtualServerRouteList
-    plural: virtualserverroutes
-    shortNames: ["vsr"]
-    singular: virtualserverroute
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    additionalPrinterColumns:
-    - jsonPath: .status.state
-      name: State
-      type: string
-    - jsonPath: .spec.host
-      name: Host
-      type: string
-    schema:
-      openAPIV3Schema:
-        properties:
-          spec:
-            properties:
-              subroutes:
-                type: array
-                items:
-                  type: object
-                  properties:
-                    path:
-                      type: string
-            type: object
-        type: object
-    subresources:
-      status: {}
-YAML
-}
-
-resource "kubectl_manifest" "virtualserver_crd" {
-  yaml_body = <<YAML
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  annotations:
-    controller-gen.kubebuilder.io/version: v0.17.1
-  name: virtualservers.k8s.nginx.org
-spec:
-  group: k8s.nginx.org
-  names:
-    kind: VirtualServer
-    listKind: VirtualServerList
-    plural: virtualservers
-    shortNames: ["vs"]
-    singular: virtualserver
-  scope: Namespaced
-  versions:
-  - name: v1
-    served: true
-    storage: true
-    additionalPrinterColumns:
-    - jsonPath: .status.state
-      name: State
-      type: string
-    schema:
-      openAPIV3Schema:
-        properties:
-          spec:
-            properties:
-              host:
-                type: string
-              routes:
-                type: array
-                items:
-                  type: object
-                  properties:
-                    path:
-                      type: string
-            type: object
-        type: object
-    subresources:
-      status: {}
-YAML
+  # Wait 10 seconds between retries (total 5 attempts)
+  retry {
+    attempts = 5
+    delay    = "10s"
+  }
 }
