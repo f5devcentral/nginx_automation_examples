5f7c917255ac1f1f0d19945cf5b1fc86be3e7c67
Akash <akashdan90@gmail.com>
2025-02-24 09:54:58 -0600

    crd

--- nap/crd.tf
+++ nap/crd.tf
@@ -1,13 +1,3 @@
-
-provider "kubectl" {
-  # Configure your Kubernetes cluster access
-  # Example configurations:
-  # config_path = "~/.kube/config"
-  # host                   = module.eks.cluster_endpoint
-  # cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  # token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 data "http" "nginx_crds" {
   url = "https://raw.githubusercontent.com/nginx/kubernetes-ingress/v4.0.1/deploy/crds.yaml"
 }
