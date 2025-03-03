f988bb0ad4369af715d5c8eae639ab249e952e7c
Akash <60446741+akananth@users.noreply.github.com>
2025-03-03 15:08:53 -0600

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -1,11 +1,33 @@
 resource "helm_release" "nginx-plus-ingress" {
-  count = 1
-  name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
+  name       = format("%s-nap-%s", local.project_prefix, local.build_suffix)
   repository = "https://helm.nginx.com/stable"
-  chart = "nginx-ingress"
-  namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
-  values = [file("./charts/nginx-plus-ingress/values.yaml")]
+  chart      = "nginx-ingress"
+  version    = "2.0.1"
+  namespace  = kubernetes_namespace.nginx-ingress.metadata[0].name
+  values     = [file("./charts/nginx-app-protect/values.yaml")]
+  timeout    = 600
+
   depends_on = [
-    kubernetes_secret.docker-registry
+    kubernetes_secret.docker-registry,
+    null_resource.nap_complete
   ]
+}
+
+resource "null_resource" "nap_complete" {
+  triggers = {
+    nap_complete = "true"
+  }
+}
+
+resource "null_resource" "copy_compiled_policy" {
+  depends_on = [
+    helm_release.nginx-plus-ingress # Ensure this runs after the Helm release
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      kubectl cp ${path.module}/nap/charts/compiled_policy.tgz nginx-ingress/${helm_release.nginx-plus-ingress.name}:/etc/app_protect/bundles/compiled_policy.tgz -n nginx-ingress
+      kubectl exec -n nginx-ingress ${helm_release.nginx-plus-ingress.name} -- ls -lh /etc/app_protect/bundles/
+    EOT
+  }
 }
