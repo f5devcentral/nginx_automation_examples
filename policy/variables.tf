832ca504bb7f9220327fb91df87b0eca531ab979
Akash <akashdan90@gmail.com>
2025-03-04 10:11:13 -0600

    policy

---
+++ policy/variables.tf
@@ -0,0 +1,30 @@
+# Terraform Cloud Organization
+variable "tf_cloud_organization" {
+  type        = string
+  description = "Terraform Cloud Organization (set in GitHub secrets)"
+}
+
+# NGINX Configuration
+variable "nginx_registry" {
+  type        = string
+  description = "NGINX Docker registry"
+  default     = "private-registry.nginx.com"
+}
+
+variable "nginx_jwt" {
+  type        = string
+  description = "JWT for pulling NGINX image"
+}
+
+variable "nginx_pwd" {
+  type        = string
+  description = "Password for NGINX (if required)"
+  default     = "none"
+}
+
+
+# SSH Key (for potential SSH-based configurations)
+variable "ssh_key" {
+  type        = string
+  description = "Unneeded for NGINX App Protect, only used for TF Cloud variable warnings"
+}
---
+++ policy/versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.15.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 3.0.2"
+    }
+  }
+}
