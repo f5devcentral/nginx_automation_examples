e2cf0ed8d3c61750723949d18c9b6f0315542a6f
Akash <60446741+akananth@users.noreply.github.com>
2025-03-05 11:02:50 -0600

    Update versions.tf

--- nap/versions.tf
+++ nap/versions.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.12.0"
     }
+    github = {
+      source = "integrations/github"
+      version = "6.6.0"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.15.0"
@@ -22,5 +26,6 @@ terraform {
       source  = "kreuzwerker/docker"
       version = ">= 3.0.2"
     }
+    
   }
 }
