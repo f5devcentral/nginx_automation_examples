f4e8ca16494dd497cce015bca75ae9f5672ca67b
Akash <akashdan90@gmail.com>
2025-03-05 16:46:20 -0600

    nap policy

--- nap/configmap.tf
+++ nap/configmap.tf
@@ -8,3 +8,14 @@ resource "kubernetes_config_map" "app_protect_bundles" {
     "compiled_policy.tgz" = var.compiled_policy_base64
   }
 }
+
+resource "kubernetes_config_map" "policy" {
+  metadata {
+    name      = "app-protect-bundles"
+    namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
+  }
+
+   data = {
+    "compiled_policy.tgz" = "${file("${path.root}/policy/compiled_policy.tgz")}"
+  }
+}
