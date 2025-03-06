bdfba04a03b0d62538f65f0f2f3cd8af775925f4
Akash <akashdan90@gmail.com>
2025-03-06 15:27:49 -0600

    update

--- nap/configmap.tf
+++ nap/configmap.tf
@@ -1,10 +1,14 @@
+data "local_file" "compiled_policy" {
+  filename = "${path.module}/compiled_policy_base64.txt"
+}
+
 resource "kubernetes_config_map" "policy" {
   metadata {
     name      = "app-protect-bundles"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
   }
 
-   data = {
-     "compiled_policy.tgz" = var.compiled_policy_base64
+  data = {
+    "compiled_policy.tgz" = data.local_file.compiled_policy.content
   }
 }
