4d2df219d3cbd2a8ec3fb37aa10f44eb0d6554d3
Akash <akashdan90@gmail.com>
2025-03-14 15:34:57 -0500

    commit

--- policy/outputs.tf
+++ policy/outputs.tf
@@ -2,3 +2,8 @@ output "copy_policy_complete" {
   description = "Indicates that the compiled policy has been copied"
   value       = null_resource.copy_compiled_policy.id
 }
+# outputs.tf
+output "aws_region" {
+  description = "AWS region"
+  value       = local.aws_region
+}
