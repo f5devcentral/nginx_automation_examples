60b71954c18080e3184017f32d145d6528e0a48a
Akash <akashdan90@gmail.com>
2025-03-14 15:13:56 -0500

    update

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -11,9 +11,6 @@ output "nap_deployment_name" {
     value = try(helm_release.nginx-plus-ingress.name, null)
     sensitive = true
 }
-output "copy_policy_complete" {
-  description = "Indicates that the compiled policy has been copied"
-  value       = null_resource.copy_compiled_policy.id
-}
+
 
 
