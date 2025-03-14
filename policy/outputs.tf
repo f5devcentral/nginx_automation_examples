1374439f98bc45ef1c3d01aa5b58763114c54f00
Akash <akashdan90@gmail.com>
2025-03-14 15:50:59 -0500

    commit

--- policy/outputs.tf
+++ policy/outputs.tf
@@ -1,9 +1,27 @@
-output "copy_policy_complete" {
-  description = "Indicates that the compiled policy has been copied"
-  value       = null_resource.copy_compiled_policy.id
-}
 # outputs.tf
 output "aws_region" {
   description = "AWS region"
   value       = local.aws_region
+}
+output "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  value       = aws_iam_access_key.example.id
+  sensitive   = true
+}
+
+output "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  value       = aws_iam_access_key.example.secret
+  sensitive   = true
+}
+
+output "aws_session_token" {
+  description = "AWS Session Token"
+  value       = aws_iam_access_key.example.sesion_token
+  sensitive   = true
+}
+
+output "copy_policy_complete" {
+  description = "Indicates that the compiled policy has been copied"
+  value       = null_resource.copy_compiled_policy.id
 }
