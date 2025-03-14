3e3ae106a7a29f15258637dea21a813218e37db2
Akash <akashdan90@gmail.com>
2025-03-14 16:24:32 -0500

    update policy

--- policy/variables.tf
+++ policy/variables.tf
@@ -22,6 +22,23 @@ variable "nginx_pwd" {
   default     = "none"
 }
 
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  description = "AWS Session Token"
+  type        = string
+  sensitive   = true
+}
 
 # SSH Key (for potential SSH-based configurations)
 variable "ssh_key" {
