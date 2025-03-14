7cc24eeeef0c1e2eaf240d1cad826054d3989750
Akash <akashdan90@gmail.com>
2025-03-14 15:01:58 -0500

    policy

--- nap/compile.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "copy_compiled_policy" {
-  provisioner "local-exec" {
-    command = <<EOT
-      kubectl cp ${path.module}/compiled_policy.tgz ${var.nginx_pod_name}:/etc/app_protect/bundles/compiled_policy.tgz -n nginx-ingress -c nginx-plus-ingress
-    EOT
-  }
-
-  depends_on = [helm_release.nginx-plus-ingress]
-}
