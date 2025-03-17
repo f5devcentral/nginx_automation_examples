5389ea45fd2f1c3e7062ba6119e72d90ad1057fc
Akash <akashdan90@gmail.com>
2025-03-17 16:36:57 -0500

    nap

--- policy/data.tf
+++ policy/data.tf
@@ -1,16 +1,11 @@
-data "tfe_outputs" "eks" {
-  organization = var.tf_cloud_organization
-  workspace = "eks"
-}
-data "tfe_outputs" "infra" {
-  organization = var.tf_cloud_organization
-  workspace = "infra"
-}
-data "tfe_outputs" "nap" {
-  organization = var.tf_cloud_organization
-  workspace = "nap"
-}
 
 data "aws_eks_cluster_auth" "auth" {
-  name = data.tfe_outputs.eks.values.cluster_name
+  name = var.cluster_name
+}
+
+data "kubernetes_service_v1" "nginx-service" {
+  metadata {
+    name      = try(format("%s-%s-controller", helm_release.nginx-plus-ingress.name, helm_release.nginx-plus-ingress.chart))
+    namespace = try(helm_release.nginx-plus-ingress.namespace)
+  }
 }
--- policy/main.tf
+++ policy/main.tf
@@ -1,36 +1,31 @@
-#Main
-#AWS Provider
+# policy/main.tf
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   token      = var.aws_session_token
 }
 
-# Create a random id
-resource "random_id" "build_suffix" {
-  byte_length = 2
-}
-
 provider "kubernetes" {
-    host = local.host
-    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token = data.aws_eks_cluster_auth.auth.token
-}
-provider "helm" {
-    kubernetes {
-        host = local.host
-        cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-        token = data.aws_eks_cluster_auth.auth.token  
-    }
-}
-provider "kubectl" {
-    host = local.host
-    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token = data.aws_eks_cluster_auth.auth.token
-    load_config_file = false
+  host                   = local.host
+  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.auth.token
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = local.host
+    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+    token                  = data.aws_eks_cluster_auth.auth.token
+  }
+}
+
+provider "kubectl" {
+  host                   = local.host
+  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.auth.token
+  load_config_file       = false
+}
 
 resource "null_resource" "copy_compiled_policy" {
   provisioner "local-exec" {
