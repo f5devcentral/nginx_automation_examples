6f93fa1f9afacb62fb6dfe93a506e068ed4c4c72
Akash <akashdan90@gmail.com>
2025-03-17 11:40:20 -0500

    napv5

--- nap/main.tf
+++ nap/main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
     region = var.aws_region
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
-    token      = var.aws_session_token
 }
 
 provider "kubernetes" {
