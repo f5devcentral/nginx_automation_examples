de98554f5ee39a7d6e776f37efec16dd6619b81c
Akash <akashdan90@gmail.com>
2025-03-17 11:12:30 -0500

    demo3

--- nap/main.tf
+++ nap/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region     = local.aws_region
+    region = var.aws_region
     access_key = var.aws_access_key_id
     secret_key = var.aws_secret_access_key
     token      = var.aws_session_token
