1aa0ef55b89af72b6f9515d171ab618b43780fa2
Akash <akashdan90@gmail.com>
2025-03-17 16:33:18 -0500

    nap

--- policy/variables.tf
+++ policy/variables.tf
@@ -1,4 +1,25 @@
+variable "project_prefix" {
+  type = string
+}
 
+variable "build_suffix" {
+  type = string
+}
+variable "aws_region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
 # NGINX Configuration
 variable "nginx_registry" {
   type        = string
@@ -17,16 +38,6 @@ variable "nginx_pwd" {
   default     = "none"
 }
 
-variable "aws_region" {
-  description = "aws region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "azs" {
-  description = "Availability Zones"
-  type        = list
-}
 
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
