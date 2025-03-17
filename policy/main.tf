c96cede4a2ca1de0734afed5ba21b6428ac7cfd7
Akash <akashdan90@gmail.com>
2025-03-17 11:40:51 -0500

    napv5

--- policy/main.tf
+++ policy/main.tf
@@ -2,9 +2,6 @@
 #AWS Provider
 provider "aws" {
   region = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  token      = var.aws_session_token
 }
 
 # Create a random id
