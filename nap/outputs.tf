f17774bad9f7cba5382bc2810c84cb5984fe0f42
Akash <akashdan90@gmail.com>
2025-03-17 10:25:39 -0500

    demo

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -11,6 +11,22 @@ output "nap_deployment_name" {
     value = try(helm_release.nginx-plus-ingress.name, null)
     sensitive = true
 }
+output "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  value       = var.aws_access_key_id  # or the actual resource/expression
+  sensitive   = true
+}
 
+output "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  value       = var.aws_secret_access_key
+  sensitive   = true
+}
+
+output "aws_session_token" {
+  description = "AWS Session Token"
+  value       = var.aws_session_token
+  sensitive   = true
+}
 
 
