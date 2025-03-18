8abb530e2a4870f79293d651d6c4a0f6d09edbe8
Akash <akashdan90@gmail.com>
2025-03-18 18:56:32 -0500

    udpate

--- oidc/outputs.tf
+++ oidc/outputs.tf
@@ -9,3 +9,6 @@ output "terraform_execution_role_arn" {
 output "terraform_state_access_policy_arn" {
   value = length(data.aws_iam_policy.existing_terraform_state_access) > 0 ? data.aws_iam_policy.existing_terraform_state_access.arn : aws_iam_policy.terraform_state_access[0].arn
 }
+output "terraform_execution_role_arn" {
+  value = aws_iam_role.terraform_execution_role.arn
+}
