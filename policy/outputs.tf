2a8f05d4e11f9aeebad43aba687697f2b0ff9ac0
Akash <akashdan90@gmail.com>
2025-03-17 22:28:20 -0500

    new

--- policy/outputs.tf
+++ policy/outputs.tf
@@ -7,24 +7,6 @@ output "cluster_name" {
   value = data.tfe_outputs.eks.values.cluster_name
 }
 
-output "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  value       = var.aws_access_key_id
-  sensitive   = true
-}
-
-output "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  value       = var.aws_secret_access_key
-  sensitive   = true
-}
-
-output "aws_session_token" {
-  description = "AWS Session Token"
-  value       = var.aws_session_token
-  sensitive   = true
-}
-
 
 output "copy_policy_complete" {
   description = "Indicates that the compiled policy has been copied"
