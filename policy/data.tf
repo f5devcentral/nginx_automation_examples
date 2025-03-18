924c2723c0abfdc31b0e229fd189c9aa287e8fb5
Akash <60446741+akananth@users.noreply.github.com>
2025-03-17 22:59:09 -0500

    Update data.tf

--- policy/data.tf
+++ policy/data.tf
@@ -1,11 +1,21 @@
-
-data "aws_eks_cluster_auth" "auth" {
-  name = var.cluster_name
+data "tfe_outputs" "infra" {
+  organization = var.tf_cloud_organization
+  workspace = "infra"
 }
-
+data "tfe_outputs" "eks" {
+  organization = var.tf_cloud_organization
+  workspace = "eks"
+}
+data "tfe_outputs" "nap" {
+  organization = var.tf_cloud_organization
+  workspace = "nap"
+}
 data "kubernetes_service_v1" "nginx-service" {
   metadata {
     name      = try(format("%s-%s-controller", helm_release.nginx-plus-ingress.name, helm_release.nginx-plus-ingress.chart))
     namespace = try(helm_release.nginx-plus-ingress.namespace)
   }
+}
+data "aws_eks_cluster_auth" "auth" {
+  name = data.tfe_outputs.eks.values.cluster_name
 }
