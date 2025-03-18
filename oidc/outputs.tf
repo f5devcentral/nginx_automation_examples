2244bcfb37ab86d4d1dc604af25771b60c7ad414
Akash <akashdan90@gmail.com>
2025-03-18 18:17:43 -0500

    oidc

--- oidc/outputs.tf
+++ oidc/outputs.tf
@@ -1,8 +1,11 @@
-# oidc/outputs.tf
 output "oidc_provider_arn" {
   value = length(aws_iam_openid_connect_provider.github_oidc) > 0 ? aws_iam_openid_connect_provider.github_oidc[0].arn : null
 }
 
 output "terraform_execution_role_arn" {
   value = length(aws_iam_role.terraform_execution_role) > 0 ? aws_iam_role.terraform_execution_role[0].arn : null
+}
+
+output "terraform_state_access_policy_arn" {
+  value = length(data.aws_iam_policy.existing_terraform_state_access) > 0 ? data.aws_iam_policy.existing_terraform_state_access.arn : aws_iam_policy.terraform_state_access[0].arn
 }
