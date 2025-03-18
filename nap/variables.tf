57d37190846793256e7fe49e91a2931b75e36932
Akash <60446741+akananth@users.noreply.github.com>
2025-03-17 22:53:34 -0500

    Update variables.tf

--- nap/variables.tf
+++ nap/variables.tf
@@ -20,27 +20,8 @@ variable "workspace_path" {
   default = ""  # Provide a default value or leave it empty
 }
 
-# nap/variables.tf
-variable "project_prefix" {
+#TF Cloud
+variable "tf_cloud_organization" {
   type = string
-}
-
-variable "build_suffix" {
-  type = string
-}
-
-variable "aws_region" {
-  type = string
-}
-
-variable "cluster_name" {
-  type = string
-}
-
-variable "cluster_endpoint" {
-  type = string
-}
-
-variable "cluster_ca_certificate" {
-  type = string
+  description = "TF cloud org (Value set in TF cloud)"
 }
