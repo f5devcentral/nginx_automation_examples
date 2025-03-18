318044e2a336aa8280e63ee44c0986b8ecd05416
Akash <akashdan90@gmail.com>
2025-03-18 14:36:19 -0500

    arn

--- infra/bootstarp.tf
+++ infra/bootstarp.tf
@@ -33,40 +33,9 @@ resource "null_resource" "state_lock_bootstrap" {
     EOT
   }
 
-  # Ensure proper creation order
-  depends_on = [aws_iam_role.terraform_execution_role]
-}
-
-# 3. State access policy with explicit dependencies
-resource "aws_iam_policy" "terraform_state_access" {
-  name        = "TerraformStateAccess"
-  description = "Access to S3 and DynamoDB for Terraform state"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket",
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DeleteItem"
-        ],
-        Resource = [
-          "arn:aws:s3:::akash-terraform-state-bucket",
-          "arn:aws:s3:::akash-terraform-state-bucket/*",
-          aws_dynamodb_table.terraform_state_lock.arn
-        ]
-      }
-    ]
-  })
-
-  # Critical dependency chain
+  # Combined dependencies in single block
   depends_on = [
-    aws_dynamodb_table.terraform_state_lock,
-    aws_iam_role.terraform_execution_role
+    aws_iam_role.terraform_execution_role,
+    aws_dynamodb_table.terraform_state_lock
   ]
 }
