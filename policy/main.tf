bbd8c38e636f2667bff1393dd68715e47b6b4b36
Akash <60446741+akananth@users.noreply.github.com>
2025-03-17 22:57:39 -0500

    Update main.tf

--- policy/main.tf
+++ policy/main.tf
@@ -1,30 +1,23 @@
-# policy/main.tf
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  token      = var.aws_session_token
+    region     = local.aws_region
 }
-
 provider "kubernetes" {
-  host                   = local.host
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.auth.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = local.host
+    host = local.host
     cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.auth.token
-  }
+    token = data.aws_eks_cluster_auth.auth.token
 }
-
+provider "helm" {
+    kubernetes {
+        host = local.host
+        cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+        token = data.aws_eks_cluster_auth.auth.token  
+    }
+}
 provider "kubectl" {
-  host                   = local.host
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.auth.token
-  load_config_file       = false
+    host = local.host
+    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+    token = data.aws_eks_cluster_auth.auth.token
+    load_config_file = false
 }
 
 resource "null_resource" "copy_compiled_policy" {
