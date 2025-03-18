e9a8d74797de7323f38e738fbc61797ace6f50b2
Akash <akashdan90@gmail.com>
2025-03-18 18:06:25 -0500

    oidc

--- oidc/outputs.tf
+++ oidc/outputs.tf
@@ -1,6 +1,6 @@
 # oidc/outputs.tf
 output "oidc_provider_arn" {
-  value = aws_iam_openid_connect_provider.github_oidc.arn
+  value = aws_iam_openid_connect_provider.github_oidc[0].arn
 }
 
 output "terraform_execution_role_arn" {
