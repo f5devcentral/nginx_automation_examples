8b21c39026dfcb406f9f4a105fb28bce2a29a358
Akash <akashdan90@gmail.com>
2025-03-18 13:21:35 -0500

    arn

---
+++ infra/dynamod.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  # Optional: Add tags if needed
+  tags = {
+    Name = "Terraform State Lock Table"
+  }
+}
