fa1fd092c8181eee95d7cd664c5801e22fd861d0
Akash <akashdan90@gmail.com>
2025-03-18 11:50:27 -0500

    demov5

--- policy/data.tf
+++ policy/data.tf
@@ -27,6 +27,10 @@ data "terraform_remote_state" "nap" {
     region = "us-east-1"                     # AWS region
   }
 }
+# Use the role ARN from infra
+locals {
+  github_actions_role_arn = data.terraform_remote_state.infra.outputs.github_actions_role_arn
+}
 
 # Keep existing data sources for Kubernetes
 data "aws_eks_cluster_auth" "auth" {
