26546d61ba3d91de257200a80189173f7d4afa50
Akash <60446741+akananth@users.noreply.github.com>
2025-03-17 23:05:15 -0500

    Update eks_cluster.tf

--- eks-cluster/eks_cluster.tf
+++ eks-cluster/eks_cluster.tf
@@ -1,76 +1,80 @@
 # eks-cluster/ekscluster.tf
+# Create EKS cluster and node groups
 resource "aws_eks_cluster" "eks-tf" {
-  name     = local.cluster_name
-  role_arn = aws_iam_role.eks-iam-role.arn
-  version  = "1.32"
 
-  vpc_config {
-    security_group_ids      = [var.internal_sg_id]
-    endpoint_private_access = true
-    endpoint_public_access  = true
-    public_access_cidrs     = ["0.0.0.0/0"]
-    subnet_ids              = var.public_subnet_ids
+ name = local.cluster_name
+ role_arn = aws_iam_role.eks-iam-role.arn
+ version = "1.32"
+
+ vpc_config {
+  security_group_ids      = flatten([aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id])
+  endpoint_private_access = true
+  endpoint_public_access  = true
+  #public_access_cidrs = [var.admin_src_addr]
+  public_access_cidrs = ["0.0.0.0/0"]
+  subnet_ids = concat([for e in aws_subnet.eks-external: e.id], [for i in aws_subnet.eks-internal: i.id])
   }
 
-  depends_on = [
-    aws_iam_role.eks-iam-role,
-  ]
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
 }
 
 resource "aws_eks_node_group" "private-node-group-1-tf" {
-  cluster_name    = aws_eks_cluster.eks-tf.name
-  node_group_name = format("%s-private-ng-1-%s", var.project_prefix, var.build_suffix)
-  node_role_arn   = aws_iam_role.workernodes.arn
-  subnet_ids      = var.private_subnet_ids
-  instance_types  = ["t3.medium"]
-
+  cluster_name  = aws_eks_cluster.eks-tf.name
+  node_group_name = format("%s-private-ng-1-%s", local.project_prefix, local.build_suffix)
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids =  [for i in aws_subnet.eks-internal: i.id]
+  instance_types = ["t3.medium"]
+ 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+   desired_size = 1
+   max_size   = 2
+   min_size   = 1
   }
 
   tags = {
-    Name = format("%s-private-ng-1-%s", var.project_prefix, var.build_suffix)
+    Name = format("%s-private-ng-1-%s", local.project_prefix, local.build_suffix)
   }
-
+ 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
-}
+ }
+
 
 resource "aws_eks_node_group" "private-node-group-2-tf" {
-  cluster_name    = aws_eks_cluster.eks-tf.name
-  node_group_name = format("%s-private-ng-2-%s", var.project_prefix, var.build_suffix)
-  node_role_arn   = aws_iam_role.workernodes.arn
-  subnet_ids      = var.private_subnet_ids
-  ami_type        = "AL2_x86_64"
-  instance_types  = ["t3.medium"]
-
+  cluster_name  = aws_eks_cluster.eks-tf.name
+  node_group_name = format("%s-private-ng-2-%s", local.project_prefix, local.build_suffix)
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids =  [for i in aws_subnet.eks-internal: i.id]
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.medium"]
+ 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+   desired_size = 1
+   max_size   = 2
+   min_size   = 1
   }
 
   tags = {
-    Name = format("%s-private-ng-2-%s", var.project_prefix, var.build_suffix)
+    Name = format("%s-private-ng-2-%s", local.project_prefix, local.build_suffix)
   }
-
+ 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
-}
+ }
 
 # Create EKS Addons
 resource "aws_eks_addon" "cluster-addons" {
-  for_each          = { for addon in var.eks_addons : addon.name => addon }
-  cluster_name      = aws_eks_cluster.eks-tf.id
-  addon_name        = each.value.name
+  for_each       = { for addon in var.eks_addons : addon.name => addon }
+  cluster_name   = aws_eks_cluster.eks-tf.id
+  addon_name     = each.value.name
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [
@@ -86,12 +90,12 @@ clusters:
 - cluster:
     server: ${aws_eks_cluster.eks-tf.endpoint}
     certificate-authority-data: ${aws_eks_cluster.eks-tf.certificate_authority[0].data}
-  name: ${aws_eks_cluster.eks-tf.arn}
+  name: ${aws_eks_cluster.eks-tf.arn}  # Use ARN as context name
 contexts:
 - context:
     cluster: ${aws_eks_cluster.eks-tf.arn}
     user: ${aws_eks_cluster.eks-tf.arn}
-  name: ${aws_eks_cluster.eks-tf.arn}
+  name: ${aws_eks_cluster.eks-tf.arn}  # Dynamic context name
 current-context: ${aws_eks_cluster.eks-tf.arn}
 kind: Config
 users:
@@ -106,7 +110,7 @@ users:
         - "--cluster-name"
         - ${aws_eks_cluster.eks-tf.name}
         - "--region"
-        - ${var.aws_region}
+        - ${local.aws_region}
 EOT
   sensitive = true
 }
