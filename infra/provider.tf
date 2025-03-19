623971d53ec838a37e36a0bd6d62a0dc391f75d2
Akash <akashdan90@gmail.com>
2025-03-19 17:15:09 -0500

    github

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,11 +2,4 @@
 provider "aws" {
   region = var.aws_region
 }
-terraform {
-  backend "s3" {
-    bucket         = "{{ .Bucket }}"
-    key            = "{{ .Key }}"
-    region         = "{{ .Region }}"
-    dynamodb_table = "{{ .DynamoDBTable }}"
-  }
-}
+
