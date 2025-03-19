9c61759fe725f3fbc67e4b0315182c63cea3864f
Akash <akashdan90@gmail.com>
2025-03-19 17:32:54 -0500

    github

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -129,23 +129,23 @@ output "internal_sg_id" {
 # S3 Bucket Details
 output "s3_bucket_created" {
   description = "Whether the S3 bucket was created."
-  value       = length(aws_s3_bucket.terraform_state_bucket.*.id) > 0 ? true : false
+  value       = length(aws_s3_bucket.terraform_state_bucket) > 0 ? true : false
 }
 
 output "s3_bucket_name" {
   description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.terraform_state_bucket.bucket
+  value       = length(aws_s3_bucket.terraform_state_bucket) > 0 ? aws_s3_bucket.terraform_state_bucket[0].bucket : null
 }
 
 # DynamoDB Table Details
 output "dynamodb_table_created" {
   description = "Whether the DynamoDB table was created."
-  value       = length(aws_dynamodb_table.terraform_state_lock.*.id) > 0 ? true : false
+  value       = length(aws_dynamodb_table.terraform_state_lock) > 0 ? true : false
 }
 
 output "dynamodb_table_name" {
   description = "The name of the DynamoDB table."
-  value       = aws_dynamodb_table.terraform_state_lock.name
+  value       = length(aws_dynamodb_table.terraform_state_lock) > 0 ? aws_dynamodb_table.terraform_state_lock[0].name : null
 }
 
 # IAM Role Output
@@ -154,13 +154,11 @@ output "iam_role_created" {
   value       = length(aws_iam_role.terraform_execution_role) > 0 ? true : false
 }
 
-
 # IAM Policy Output
 output "iam_policy_created" {
   description = "Whether the IAM policy was created."
   value       = length(aws_iam_policy.terraform_state_access) > 0 ? true : false
 }
-
 
 output "iam_role_name" {
   description = "The name of the IAM role."
