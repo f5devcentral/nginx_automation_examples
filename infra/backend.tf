7e4a70ba60c7a774d6d91b18315b723f873b7057
Akash <akashdan90@gmail.com>
2025-03-19 10:48:06 -0500

    depends

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "akash-terraform-state-bucket"
-    key    = "infra/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    use_lockfile = true
-  }
-}
