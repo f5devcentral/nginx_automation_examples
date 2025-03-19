244bf2c4ad213265186e0fee069926b60066359e
Akash <akashdan90@gmail.com>
2025-03-18 22:45:47 -0500

    nap

--- nap/variables.tf
+++ nap/variables.tf
@@ -5,19 +5,11 @@ variable "nginx_registry" {
   default     = "private-registry.nginx.com"
 }
 
-variable "nginx_jwt" {
-  type        = string
-  description = "JWT for pulling NGINX image"
-}
-
 variable "nginx_pwd" {
   type        = string
   description = "Password for NGINX (if required)"
   default     = "none"
 }
-variable "workspace_path" {
-  type    = string
-  default = ""  # Provide a default value or leave it empty
-}
+
 
 
