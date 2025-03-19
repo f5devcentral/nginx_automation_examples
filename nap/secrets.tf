eb693323ef85ec5eb3269dc028414bf5a713947e
Akash <akashdan90@gmail.com>
2025-03-18 23:31:32 -0500

    nap

--- nap/secrets.tf
+++ nap/secrets.tf
@@ -12,22 +12,22 @@ resource "kubernetes_secret" "nginx_license" {
 }
 
 resource "kubernetes_secret" "docker-registry" {
-    metadata {
-        name      = "regcred"
-        namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
-    }
+  metadata {
+    name      = "regcred"
+    namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
+  }
 
-    type = "kubernetes.io/dockerconfigjson"
+  type = "kubernetes.io/dockerconfigjson"
 
-    data = {
-        ".dockerconfigjson" = jsonencode({
-            auths = {
-                "${var.nginx_registry}" = {
-                    "username" = var.nginx_jwt,
-                    "password" = var.nginx_pwd,
-                    "auth"     = base64encode("${var.nginx_jwt}:${var.nginx_pwd}")
-                }
-            }
-        })
-    }
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.nginx_registry}" = {
+          "username" = var.nginx_jwt,
+          "password" = "",  # Set password to an empty string
+          "auth"     = base64encode("${var.nginx_jwt}:")  # Only JWT token is used
+        }
+      }
+    })
+  }
 }
