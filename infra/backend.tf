a28b605ca17a50d1f28a15802484e974f34fe75d
Akash <akashdan90@gmail.com>
2025-03-19 17:30:09 -0500

    github

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    # These values will be overridden by the -backend-config flags
-    bucket         = "dummy"
-    key            = "dummy"
-    region         = "dummy"
-    dynamodb_table = "dummy"
-  }
-}
