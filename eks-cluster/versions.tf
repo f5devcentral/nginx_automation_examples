4022c7a2cb7d8142b04a17fe93a776e7e56bb1d9
Akash <akashdan90@gmail.com>
2025-03-19 10:06:34 -0500

    demo

--- eks-cluster/versions.tf
+++ eks-cluster/versions.tf
@@ -12,6 +12,7 @@ terraform {
     key            = "eks-cluster/terraform.tfstate"       # Path to state file
     region         = "us-east-1"                     # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
-    encrypt        = true                            # Encrypt state file at rest
+    encrypt        = true  
+                           
   }
 }
