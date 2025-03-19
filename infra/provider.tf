31739c271b5470cea73d9b1be7ee08e56cbf4618
Akash <akashdan90@gmail.com>
2025-03-19 17:04:51 -0500

    github

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,11 +3,7 @@ provider "aws" {
   region = var.aws_region
 }
 terraform {
-  backend "s3" {
-    bucket         = "akash-terraform-state-bucket" # The bucket must already exist
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-table" # Use DynamoDB for state locking
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
