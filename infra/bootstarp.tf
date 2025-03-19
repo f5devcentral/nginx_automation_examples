d71a925c9fbfbf5b7c6b783fb64ffda62905723a
Akash <akashdan90@gmail.com>
2025-03-19 12:54:51 -0500

    nap

--- infra/bootstarp.tf
+++ infra/bootstarp.tf
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-=======
-# Remove the S3 bucket creation
-# Remove this block:
-# resource "aws_s3_bucket" "state" {
-#   bucket = "akash-terraform-state-bucket"
-#
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-#
-#   versioning {
-#     enabled = true
-#   }
-#
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
-
->>>>>>> origin/apply-nap
 # Use the existing S3 bucket for the backend
 terraform {
   backend "s3" {
