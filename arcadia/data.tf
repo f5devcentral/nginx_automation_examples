39ee3418fdb224c64e43327975f3bc5bb679b527
Akash <akashdan90@gmail.com>
2025-03-19 23:27:49 -0500

    eks

--- arcadia/data.tf
+++ arcadia/data.tf
@@ -22,7 +22,7 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
     bucket = "akash-terraform-state-bucket"  # Your S3 bucket name
-    key    = "eks/terraform.tfstate"  # Path to EKS state file
+    key    = "eks-cluster/terraform.tfstate"  # Path to EKS state file
     region = "us-east-1"                     # AWS region
   }
 }
