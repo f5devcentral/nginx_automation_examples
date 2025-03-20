4ea22179a196a9d2d317fe82ac58d3765bd9dff1
Akash <akashdan90@gmail.com>
2025-03-20 14:12:24 -0500

    Copied infra and eks-cluster folders from apply-nap to destroy-nap

--- nap/variables.tf
+++ nap/variables.tf
@@ -11,3 +11,14 @@ variable "nginx_pwd" {
   default     = "none"
 }
 
+variable "workspace_path" {
+  description = "The path to the workspace directory"
+  type        = string
+}
+
+variable "nginx_jwt" {
+  description = "The JWT token for NGINX"
+  type        = string
+  sensitive   = true  # Mark as sensitive to avoid exposing it in logs
+}
+
