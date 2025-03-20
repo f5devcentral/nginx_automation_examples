00e416ce48661f5516360e7bab6d383e96cb650f
Akash <60446741+akananth@users.noreply.github.com>
2025-03-20 09:28:12 -0500

    Delete nic directory

--- nic/prometheus.tf
+++
@@ -1,7 +0,0 @@
-resource "helm_release" "prometheus" {
-  name = format("%s-pro-%s", lower(local.project_prefix), lower(local.build_suffix)) 
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart = "prometheus"
-  namespace = kubernetes_namespace.monitoring.metadata[0].name
-  values = [file("./charts/prometheus/values.yaml")]
-}
