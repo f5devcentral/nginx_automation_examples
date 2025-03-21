cd9086c46f19c97c091d99c9e550eecd29d63dba
Akash <60446741+akananth@users.noreply.github.com>
2025-03-21 10:19:26 -0500

    Update versions.tf

--- policy/versions.tf
+++ policy/versions.tf
@@ -14,18 +14,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.12.0"
     }
-    github = {
-      source = "integrations/github"
-      version = "6.6.0"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.15.0"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 3.0.2"
-    }
-    
   }
 }
