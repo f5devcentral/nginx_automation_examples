6ea86f896fd5a4b7f8131978676bc83fa4d3d46f
Akash <60446741+akananth@users.noreply.github.com>
2025-03-21 10:45:39 -0500

    Update versions.tf

--- policy/versions.tf
+++ policy/versions.tf
@@ -10,10 +10,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.23.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.12.0"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.15.0"
