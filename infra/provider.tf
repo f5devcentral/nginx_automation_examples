9ac50c7dfc6c81adc62acea65f8b745fe8bfde11
Akash <akashdan90@gmail.com>
2025-03-23 21:15:35 -0500

    infra

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,13 +3,3 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "akash-terraform-state-bucket"  # Use the existing bucket
-    key            = "infra/terraform.tfstate"       # State file path
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"          # Existing DynamoDB table
-    encrypt        = true
-  }
-}
-
