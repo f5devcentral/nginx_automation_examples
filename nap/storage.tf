86b54e51f71e58d11eb31bd7d6b61c714b6cf3c5
Akash <akashdan90@gmail.com>
2025-03-24 11:02:24 -0500

    demo

--- nap/storage.tf
+++ nap/storage.tf
@@ -1,3 +1,19 @@
+
+resource "kubernetes_service_account" "ebs_csi_controller" {
+  depends_on = [
+    data.terraform_remote_state.eks.outputs.cluster_endpoint,
+    data.terraform_remote_state.eks.outputs.oidc_provider_arn
+  ]
+
+  metadata {
+    name      = "ebs-csi-controller-sa"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = data.terraform_remote_state.eks.outputs.ebs_csi_driver_role_arn
+    }
+  }
+}
+
 resource "kubernetes_storage_class_v1" "aws_csi" {
   metadata {
     name = "ebs-sc"
