f4e45263e9e297ecd607e1de1f6207f8e131f0ed
Akash <akashdan90@gmail.com>
2025-03-23 22:36:05 -0500

    destroy-2

--- nap/variables.tf
+++ nap/variables.tf
@@ -11,6 +11,11 @@ variable "nginx_pwd" {
   default     = "none"
 }
 
+variable "workspace_path" {
+  description = "The path to the workspace directory"
+  type        = string
+}
+
 variable "nginx_jwt" {
   description = "The JWT token for NGINX"
   type        = string
