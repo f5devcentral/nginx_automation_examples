3fbbffe68af3b00e3a031e62e65f26a3e09c754b
Akash <60446741+akananth@users.noreply.github.com>
2025-03-25 22:02:15 -0500

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "akash-terraform-state-bucket"  # Your S3 bucket name
+    bucket         = "your-unique-bucket-name"       # Your S3 bucket name
     key            = "infra/terraform.tfstate"       # Path to state file
     region         = "us-east-1"                     # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
