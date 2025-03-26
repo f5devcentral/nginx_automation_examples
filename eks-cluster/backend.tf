46764c1fa8860d6de706ae69dfc9becf3dc9b83a
Akash <60446741+akananth@users.noreply.github.com>
2025-03-25 22:29:11 -0500

    Update backend.tf

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,10 +1,9 @@
-terrafrom {
-    backend "s3" {
-    bucket         = "your-unique-bucket-name" # Replace with your actual bucket name
-    key            = "eks-cluster/terraform.tfstate"       # Path to state file
+terraform {
+  backend "s3" {
+    bucket         = "your-unique-bucket-name"       # Your S3 bucket name
+    key            = "eks/terraform.tfstate"       # Path to state file
     region         = "us-east-1"                     # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
-    encrypt        = true  
-                           
+    encrypt        = true                        
   }
 }
