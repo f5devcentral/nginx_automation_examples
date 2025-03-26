16e03e3f1860e11574155a20b3c46651a1093c85
Akash <60446741+akananth@users.noreply.github.com>
2025-03-25 23:05:46 -0500

    Update locals.tf

--- arcadia/locals.tf
+++ arcadia/locals.tf
@@ -1,7 +1,7 @@
 locals {
   project_prefix          = data.terraform_remote_state.infra.outputs.project_prefix
   aws_region             = data.terraform_remote_state.infra.outputs.aws_region
-  external_name          = try(data.terraform_remote_state.nap.outputs.external_name, "arcadia-cd-demo.sr.f5-cloud-demo.com")
+  external_name          = try(data.terraform_remote_state.nap.outputs.external_name)
   host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
   cluster_ca_certificate = data.terraform_remote_state.eks.outputs.kubeconfig-certificate-authority-data
   cluster_name           = data.terraform_remote_state.eks.outputs.cluster_name
