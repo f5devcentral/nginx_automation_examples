5c7133350c3431a3e532ec33be2ceab2fbec2525
Akash <60446741+akananth@users.noreply.github.com>
2025-03-25 22:44:28 -0500

    Update data.tf

--- eks-cluster/data.tf
+++ eks-cluster/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "infra" {
   backend = "s3"
   config = {
-    bucket = "akash-terraform-state-bucket"  # Your S3 bucket name
+    bucket =  "your-unique-bucket-name"       # Your S3 bucket name
     key    = "infra/terraform.tfstate"       # Path to infra's state file
     region = "us-east-1"                     # AWS region
   }
